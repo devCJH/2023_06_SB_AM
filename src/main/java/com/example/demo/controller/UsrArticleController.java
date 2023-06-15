abc68fc9a39d1805b24dbf350e08b33ff63ede21
devCJH <http-@naver.com>
2023-06-15 21:49:06 +0900

    writeArticle

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -18,11 +18,21 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
+		
+		makeTestData();
 	}
 	
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
+	private void makeTestData() {
+		for(int i = 1; i <= 10; i++) {
+			
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			writeArticle(title, body);
+		}
+	}
+
+	private Article writeArticle(String title, String body) {
 		
 		int id = this.lastArticleId + 1;
 		this.lastArticleId = id;
@@ -34,6 +44,15 @@ public class UsrArticleController {
 		return article;
 	}
 	
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		
+		Article article = writeArticle(title, body);
+		
+		return article;
+	}
+
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
