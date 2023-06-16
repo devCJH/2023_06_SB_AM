24ca736db1c5fd5455a63cf27a7b4b5e0766daec
devCJH <http-@naver.com>
2023-06-16 20:56:51 +0900

    delete, modify, detail

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -32,6 +32,7 @@ public class UsrArticleController {
 		}
 	}
 
+	// 서비스 메서드
 	private Article writeArticle(String title, String body) {
 		
 		int id = this.lastArticleId + 1;
@@ -44,6 +45,30 @@ public class UsrArticleController {
 		return article;
 	}
 	
+	private Article getArticleById(int id) {
+		
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+		
+		return null;
+	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticleById(id);
+		
+		article.setTitle(title); 
+		article.setBody(body); 
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticleById(id);
+		articles.remove(article);
+	}
+	
+	// 액션 메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -57,5 +82,48 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> getArticles() {
 		return this.articles;
+	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticle(int id) {
+		
+		Article foundArticle = getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
+		
+		return foundArticle;
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		
+		Article foundArticle = getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
+		
+		modifyArticle(id, title, body);
+		
+		return id + "번 게시글을 수정했습니다";
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		
+		Article foundArticle = getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
+		
+		deleteArticle(id);
+		
+		return id + "번 게시글을 삭제했습니다";
 	}
 }
