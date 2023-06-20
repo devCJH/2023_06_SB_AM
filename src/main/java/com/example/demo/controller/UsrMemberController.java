8d8b8f243eb3ca29d6ffec2c458905dfda3eadd4
devCJH <http-@naver.com>
2023-06-20 21:31:28 +0900

    Util.empty()

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.service.MemberService;
-import com.example.demo.vo.Member;
+import com.example.demo.util.Util;
 
 @Controller
 public class UsrMemberController {
@@ -21,7 +21,26 @@ public class UsrMemberController {
 	// 액션 메서드
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+		
+		if(Util.empty(loginId)) {
+			return "아이디를 입력해주세요";
+		}
+		if(Util.empty(loginPw)) {
+			return "비밀번호를 입력해주세요";
+		}
+		if(Util.empty(name)) {
+			return "이름을 입력해주세요";
+		}
+		if(Util.empty(nickname)) {
+			return "닉네임을 입력해주세요";
+		}
+		if(Util.empty(cellphoneNum)) {
+			return "전화번호를 입력해주세요";
+		}
+		if(Util.empty(email)) {
+			return "이메일을 입력해주세요";
+		}
 		
 		memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
 		
